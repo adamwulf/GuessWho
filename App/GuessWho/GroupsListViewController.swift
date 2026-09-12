cba270b248f86a5964f928c7690a2668a589d4e5
Adam Wulf <adam@adamwulf.me>
2026-09-11 22:40:21 -0500

    Use symmetric list selection insets

--- App/GuessWho/GroupsListViewController.swift
+++ App/GuessWho/GroupsListViewController.swift
@@ -553,7 +553,7 @@ private final class GroupCell: UITableViewCell {
         if state.isSelected || state.isHighlighted {
             background.backgroundColor = .tintColor
             background.cornerRadius = 8
-            background.backgroundInsets = NSDirectionalEdgeInsets(top: 2, leading: 10, bottom: 2, trailing: 16)
+            background.backgroundInsets = NSDirectionalEdgeInsets(top: 2, leading: 10, bottom: 2, trailing: 10)
         }
         backgroundConfiguration = background
 
